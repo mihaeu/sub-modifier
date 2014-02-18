f8cfebe5af00b6246ae20c329a5dadd9a230509e
Michael Haeuslmann <haeuslmann@gmail.com>
2014-02-18 11:43:32 +0330

    Refactored main class and brought test coverage up to 100%.

--- src/Mihaeu/SubModifier/SubModifier.php
+++ src/Mihaeu/SubModifier/SubModifier.php
@@ -2,45 +2,69 @@
 
 namespace Mihaeu\SubModifier;
 
-define('S_TO_MS', 1000);
+define('S_TO_MS',   1000);
 define('MIN_TO_MS', S_TO_MS * 60);
-define('H_TO_MS', MIN_TO_MS * 60);
+define('H_TO_MS',   MIN_TO_MS * 60);
 
 class SubModifier
 {
     public $indexRegex = '/^\d+$/';
-    public $timeRegex = '/(?<from>\d\d:\d\d:\d\d,\d\d\d) --> (?<to>\d\d:\d\d:\d\d,\d\d\d)/';
-    public $srtTimeFormat = 'H:i:s,u';
+    public $srtTimeLineRegex = '/(?<from>\d\d:\d\d:\d\d,\d\d\d) --> (?<to>\d\d:\d\d:\d\d,\d\d\d)/';
+    
+    public $srtTimeFormat = '%02d:%02d:%02d,%03d';
+    public $srtTimeRegex = '/(?<neg>-)?(?<h>\d\d):(?<min>[0-5][0-9]):(?<s>[0-5][0-9]),(?<ms>\d\d\d)/';
 
-    public function parseSrtFile($srtFile, $offsetSrtTime)
+    /**
+     * Modifies the timestamps in an .srt subtitle file.
+     * 
+     * @param  string $srtFile
+     * @param  string $offsetSrtTime in srt time format
+     * @return void
+     */
+    public function modifySrtFile($srtFile, $offsetSrtTime)
     {
+        if (!$this->isSrtTime($offsetSrtTime)) {
+            throw new \InvalidArgumentException(
+                "$offsetSrtTime is not a valid SRT time format (eg. 01:02:03,004).\n"
+            );
+        }
+        if (!is_readable($srtFile)) {
+            throw new \InvalidArgumentException(
+                "$srtFile file does not exist or is not readable.\n"
+            );
+        }
+
         $entryNumber = 1;
         $offsetTimeMs = $this->srtTimeToMs($offsetSrtTime);
 
-        $contents = file_get_contents($srtFile);
+        $contents = $this->normalizeLinefeeds(file_get_contents($srtFile));
+        $output = [];
         foreach (explode("\n", $contents) as $line) {
             $matches = [];
             if (preg_match($this->indexRegex, $line)) {
-                echo "$entryNumber\n\r";
+                $output[] = $entryNumber;
                 ++$entryNumber;
-            } else if (preg_match($this->timeRegex, $line, $matches)) {
+            } else if (preg_match($this->srtTimeLineRegex, $line, $matches)) {
                 $fromMs = $this->srtTimeToMs($matches['from']) - $offsetTimeMs;
                 $toMs   = $this->srtTimeToMs($matches['to']) - $offsetTimeMs;
-                echo $this->msToSrtFormat($fromMs).' --> '.$this->msToSrtFormat($toMs)."\n\r";
+                $output[] = $this->msToSrtFormat($fromMs).' --> '.$this->msToSrtFormat($toMs);
             } else {
-                echo "$line\n\r";
+                $output[] = $line;
             }
         }
+        return implode(PHP_EOL, $output);
     }
 
+    /**
+     * Converts the srt time format into ms.
+     * 
+     * @param  string $srtFormatString
+     * @return int
+     */
     public function srtTimeToMs($srtFormatString)
     {
         $matches = [];
-        preg_match(
-            '/(?<neg>-)?(?<h>\d\d):(?<min>\d\d):(?<s>\d\d),(?<ms>\d\d\d)/',
-            $srtFormatString,
-            $matches
-        );
+        preg_match($this->srtTimeRegex, $srtFormatString, $matches);
         $neg = ($matches['neg'] === '-') ? -1 : 1;
         return $neg * 
             ($matches['ms'] 
@@ -49,20 +73,50 @@ class SubModifier
             + ($matches['h'] * H_TO_MS));
     }
 
+    /**
+     * Converts ms to the srt time format.
+     * 
+     * @param  int $ms
+     * @return string
+     */
     public function msToSrtFormat($ms)
     {
-        assert(is_numeric($ms));
-
         $h   = floor($ms / H_TO_MS);
         $min = floor(($ms - ($h * H_TO_MS)) / MIN_TO_MS);
         $s   = floor(($ms - ($h * H_TO_MS) - ($min * MIN_TO_MS)) / S_TO_MS);
         $ms  = $ms - ($h * H_TO_MS) - ($min * MIN_TO_MS) - ($s * S_TO_MS);
 
-        return sprintf('%02d:%02d:%02d,%03d', $h, $min, $s, $ms);
+        return sprintf($this->srtTimeFormat, $h, $min, $s, $ms);
     }
 
-    public function tests()
+    /**
+     * Tests if a given string follows the srt time format: h:min:s,ms
+     * 
+     * @param  strin  $srtTime
+     * @return bool
+     */
+    public function isSrtTime($srtTime)
     {
-        
+        return 1 === preg_match($this->srtTimeRegex, $srtTime);
+    }
+
+    /**
+     * Normalizes the different linefeeds of different OS (Mac: \r, Windows: \r\n) to
+     * contain only UNIX style linefeeds (\n).
+     *
+     * Order matters! First remove windows, then Mac linefeeds, otherwise we might
+     * end up having twice the linefeeds.
+     * 
+     * @param string $content
+     * @return string
+     */
+    public function normalizeLinefeeds($content)
+    {
+        $contentWithoutWindowsLinefeeds =
+            str_replace("\r\n", "\n", $content);
+        $contentWithoutMacLinefeeds =
+            str_replace("\r", "\n", $contentWithoutWindowsLinefeeds);
+
+        return $contentWithoutMacLinefeeds;
     }
 }
--- tests/Mihaeu/SubModifier/SubModifierTest.php
+++ tests/Mihaeu/SubModifier/SubModifierTest.php
@@ -14,6 +14,35 @@ class SubModifierTest extends PHPUnit_Framework_TestCase
         $this->subModifier = new SubModifier();
     }
 
+    public function testConvertsMsToSrtTimeFormat()
+    {
+        $this->assertEquals('00:00:01,001', $this->subModifier->msToSrtFormat('1001'));
+    }
+
+    public function testConvertsSrtTimeFormatToMs()
+    {
+        $this->assertEquals(1001, $this->subModifier->srtTimeToMs('00:00:01,001'));
+        
+        $h   = 1 * 60 * 60 * 1000;
+        $min = 2 * 60 * 1000;
+        $s   = 3 * 1000;
+        $ms  = 4;
+        $this->assertEquals($h + $min + $s + $ms, $this->subModifier->srtTimeToMs('01:02:03,004'));
+    }
+
+    public function testValidatesSrtTimeFormats()
+    {
+        $this->assertTrue($this->subModifier->isSrtTime('01:02:03,984'));
+
+        // legal characters but semantically wrong
+        $this->assertFalse($this->subModifier->isSrtTime('01:99:03,004'));
+        $this->assertFalse($this->subModifier->isSrtTime('01:03:88,004'));
+
+        $this->assertFalse($this->subModifier->isSrtTime('1:2:3,04'));
+        $this->assertFalse($this->subModifier->isSrtTime('01:02:03.004'));
+        $this->assertFalse($this->subModifier->isSrtTime('01.02.03'));
+    }
+
     public function testConvertsSrtTimeToMsAndBack()
     {
         $srtTimeBefore = '01:01:01,001';
@@ -49,5 +78,87 @@ class SubModifierTest extends PHPUnit_Framework_TestCase
         $msAfter = $this->subModifier->srtTimeToMs($srtTimeAfter);
         $diff = $this->subModifier->msToSrtFormat($msBefore - $msAfter);
         $this->assertEquals($diff, '01:02:03,087');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage file
+     */
+    public function testDoesntAcceptBadSrtFile()
+    {
+        $legalSrtOffset = '00:00:00,000';
+        $badSrtFile = '/tmp/subtitleFileWhichDoesntExist';
+        $this->subModifier->modifySrtFile($badSrtFile, $legalSrtOffset);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage time format
+     */
+    public function testDoesntAcceptBadSrtFormat()
+    {
+        $goodSrtFile = $temp_file = tempnam(sys_get_temp_dir(), 'Tux');
+        $badSrtOffset = '00-00-00-000';
+        $this->subModifier->modifySrtFile($goodSrtFile, $badSrtOffset);
+    }
+
+    public function testNormalizesLinefeedsOfInputSrtFile()
+    {
+        foreach (["\r\n", "\r", "\n"] as $lf) {
+            $goodSrtSubtitle = 
+                "1$lf".
+                "00:00:01,000 --> 00:00:04,074$lf".
+                "Subtitles downloaded from www.OpenSubtitles.org$lf".
+                "$lf".
+                "2$lf".
+                "00:00:38,437 --> 00:00:40,639$lf".
+                "All started in my old forge$lf".
+                "in Ventura, California";
+            $expectedNormalizedSrtSubtitle =
+                "1".PHP_EOL.
+                "00:00:01,000 --> 00:00:04,074".PHP_EOL.
+                "Subtitles downloaded from www.OpenSubtitles.org".PHP_EOL.
+                "".PHP_EOL.
+                "2".PHP_EOL.
+                "00:00:38,437 --> 00:00:40,639".PHP_EOL.
+                "All started in my old forge".PHP_EOL.
+                "in Ventura, California";
+            $actualNormalizedSrtSubtitle =
+                $this->subModifier->normalizeLinefeeds($goodSrtSubtitle);
+            $this->assertEquals($expectedNormalizedSrtSubtitle, $actualNormalizedSrtSubtitle);
+        }
+    }
+
+    public function testDelaysSubtitlesByGivenTime()
+    {
+        $srtSubtitleBefore =
+            "1".PHP_EOL.
+            "00:00:01,000 --> 00:00:04,074".PHP_EOL.
+            "Subtitles downloaded from www.OpenSubtitles.org".PHP_EOL.
+            "".PHP_EOL.
+            "2".PHP_EOL.
+            "00:00:38,437 --> 00:00:40,639".PHP_EOL.
+            "All started in my old forge".PHP_EOL.
+            "in Ventura, California";
+        $expectedSrtSubtitleAfterOneMinuteDelay =
+            "1".PHP_EOL.
+            "00:01:01,000 --> 00:01:04,074".PHP_EOL.
+            "Subtitles downloaded from www.OpenSubtitles.org".PHP_EOL.
+            "".PHP_EOL.
+            "2".PHP_EOL.
+            "00:01:38,437 --> 00:01:40,639".PHP_EOL.
+            "All started in my old forge".PHP_EOL.
+            "in Ventura, California";
+        $goodSrtFile = tempnam(sys_get_temp_dir(), 'Tux');
+        file_put_contents($goodSrtFile, $srtSubtitleBefore);
+
+        $actualSrtSubtitleAfterOneMinuteDelay = 
+            $this->subModifier->modifySrtFile($goodSrtFile, '-00:01:00,000');;
+
+        unlink($goodSrtFile);
+        $this->assertEquals(
+            $expectedSrtSubtitleAfterOneMinuteDelay,
+            $actualSrtSubtitleAfterOneMinuteDelay
+        );
     }
 }
